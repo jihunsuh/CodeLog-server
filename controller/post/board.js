69ad2661d464459a5f47916b05607ea8514a0c76
서지훈 <jihunsuh12@gmail.com>
2020-02-12 18:03:20 +0900

    add ts file in controller/post/

--- controller/post/board.js
+++ controller/post/board.js
@@ -1,63 +1,124 @@
-const asyncHandler = require('express-async-handler');
-
-const { isValid } = require('../../utils/token');
-const { postings, tags } = require('../../services');
-
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var asyncHandler = require('express-async-handler');
+var isValid = require('../../utils/token').isValid;
+var _a = require('../../services'), postings = _a.postings, tags = _a.tags;
 module.exports = {
-  post: asyncHandler(async (req, res) => {
-    const { theme, title, content, selected_tags } = req.body;
-    const { token } = req.cookies;
-
-    let decodeData = await isValid(token);
-    if (!decodeData.isValid) {
-      res.status(403).send('login required');
-      return;
-    }
-    const userid = decodeData.userData.id;
-
-    let postresult = await postings.create(userid, title, content, 0, theme);
-    if (!postresult.success) {
-      res.status(404).send("i can't upload your postings");
-      return;
-    }
-    const postid = postresult.payload.id;
-
-    let tagids = [];
-    for (let i = 0; i < selected_tags.length; i++) {
-      let findresult = await tags.findByName(selected_tags[i]);
-      if (!findresult.success) {
-        res
-          .status(404)
-          .send("i saved your postings, but i can't find your tag " + selected_tags[i]);
-        return;
-      }
-      tagids.push(findresult.payload.id);
-    }
-
-    let addresult = await postings.addTags(postid, tagids);
-    if (!addresult.success) {
-      res.status(404).send("i saved your postings, but i can't put your tags in");
-      return;
-    }
-
-    res.status(201).send('Posting successfully created!');
-  }),
-  get: asyncHandler(async (req, res) => {
-    const { id } = req.body;
-
-    let findresult = await postings.find(id);
-    if (!findresult.success) {
-      res.status(404).send("i can't find your postings");
-      return;
-    }
-    let postingInfo = findresult.payload;
-
-    let tagfindresult = await tags.findNamesByPostId(id);
-    if (!tagfindresult.success) {
-      res.status(404).send("i found your postings, but i can't find posting tags");
-      return;
-    }
-    postingInfo['tags'] = tagfindresult.payload;
-    res.status(200).send(postingInfo);
-  }),
+    post: asyncHandler(function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+        var _a, theme, title, content, selected_tags, token, decodeData, userid, postresult, postid, tagids, i, findresult, addresult;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    _a = req.body, theme = _a.theme, title = _a.title, content = _a.content, selected_tags = _a.selected_tags;
+                    token = req.cookies.token;
+                    return [4 /*yield*/, isValid(token)];
+                case 1:
+                    decodeData = _b.sent();
+                    if (!decodeData.isValid) {
+                        res.status(403).send('login required');
+                        return [2 /*return*/];
+                    }
+                    userid = decodeData.userData.id;
+                    return [4 /*yield*/, postings.create(userid, title, content, 0, theme)];
+                case 2:
+                    postresult = _b.sent();
+                    if (!postresult.success) {
+                        res.status(404).send("i can't upload your postings");
+                        return [2 /*return*/];
+                    }
+                    postid = postresult.payload.id;
+                    tagids = [];
+                    i = 0;
+                    _b.label = 3;
+                case 3:
+                    if (!(i < selected_tags.length)) return [3 /*break*/, 6];
+                    return [4 /*yield*/, tags.findByName(selected_tags[i])];
+                case 4:
+                    findresult = _b.sent();
+                    if (!findresult.success) {
+                        res
+                            .status(404)
+                            .send("i saved your postings, but i can't find your tag " + selected_tags[i]);
+                        return [2 /*return*/];
+                    }
+                    tagids.push(findresult.payload.id);
+                    _b.label = 5;
+                case 5:
+                    i++;
+                    return [3 /*break*/, 3];
+                case 6: return [4 /*yield*/, postings.addTags(postid, tagids)];
+                case 7:
+                    addresult = _b.sent();
+                    if (!addresult.success) {
+                        res.status(404).send("i saved your postings, but i can't put your tags in");
+                        return [2 /*return*/];
+                    }
+                    res.status(201).send('Posting successfully created!');
+                    return [2 /*return*/];
+            }
+        });
+    }); }),
+    get: asyncHandler(function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+        var id, findresult, postingInfo, tagfindresult;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    id = req.body.id;
+                    return [4 /*yield*/, postings.find(id)];
+                case 1:
+                    findresult = _a.sent();
+                    if (!findresult.success) {
+                        res.status(404).send("i can't find your postings");
+                        return [2 /*return*/];
+                    }
+                    postingInfo = findresult.payload;
+                    return [4 /*yield*/, tags.findNamesByPostId(id)];
+                case 2:
+                    tagfindresult = _a.sent();
+                    if (!tagfindresult.success) {
+                        res.status(404).send("i found your postings, but i can't find posting tags");
+                        return [2 /*return*/];
+                    }
+                    postingInfo['tags'] = tagfindresult.payload;
+                    res.status(200).send(postingInfo);
+                    return [2 /*return*/];
+            }
+        });
+    }); })
 };
